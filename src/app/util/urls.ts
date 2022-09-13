6bec388e9ab909ec1e2e68564ff797f31e860c8e
Rachel Schoneveld <rcschoneveld@hotmail.com>
2022-09-13 14:29:47 +0200

    urls

--- src/app/util/urls.ts
+++ src/app/util/urls.ts
@@ -1,2 +1,2 @@
-export const insiderSentimentUrl = (symbol: string, startDate: string, endDate: string) => `/api/stock/insider-sentiment?symbol=${symbol}&from=${startDate}&to=${endDate}`;
-export const realTimeQuoteUrl = (symbol : string) => `/api/quote?symbol=${symbol}`;
+export const insiderSentimentUrl = (symbol: string, startDate: string, endDate: string) => `https://finnhub.io/api/v1/stock/insider-sentiment?symbol=${symbol}&from=${startDate}&to=${endDate}`;
+export const realTimeQuoteUrl = (symbol : string) => `https://finnhub.io/api/v1/quote?symbol=${symbol}`;
